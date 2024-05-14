0d9a9f6796cb81694ab2695c4ed1bf80e3bfd651
antonnprince <antonprince95@gmail.com>
2024-05-14 09:48:14 +0530

    progress bar done

--- src/App.jsx
+++ src/App.jsx
@@ -45,13 +45,13 @@ const content = [
             <img src={target} className='w-12 h-12  my-2' />
             <div className='flex flex-col space-y-1 mx-2'>
               <h1 className='text-sm'>Your Daily Goals Almost Done</h1>
-              <p className='text-sm'>4 out of 5  completed</p>
+              <p className='text-sm'>{snap.count} out of 5  completed</p>
               
                 <div className="w-full bg-[#358cdd] rounded-full h-1 ">
-                  <div className="bg-[#bed7ef] h-1 rounded-full" style={{width: "45%"}}></div>
+                  <div className="bg-[#bed7ef] h-1 rounded-full" style={{width: `${(snap.count/5)*100}%`}}></div>
                 </div> 
 
-                <h3 className='text-right text-xs'>80%</h3>
+                <h3 className='text-right text-xs'>{(snap.count/5)*100}%</h3>
             </div>
         </div>
 
